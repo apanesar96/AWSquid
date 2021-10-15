2642e9cfe49f83587f10a3f8fc6b52aa94d6f0ed
SteeleSR <67695962+SteeleSR@users.noreply.github.com>
2021-10-15 11:31:11 +0100

    Delete .github/workflows/backendwebsiteservice directory

--- .github/workflows/backendwebsiteservice/src/test/java/com/codurance/cephalopodwebsite/CephalopodFeatureRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codurance.cephalopodwebsite;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CephalopodFeatureRepository extends JpaRepository<String,Long>{
-}
