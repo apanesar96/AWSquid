1a6355513147245fa24354f273f049d3030d34c0
Aman Panesar <amanpanesar@Amans-MacBook-Pro.local>
2021-10-17 10:05:39 +0100

    disable database url driver

--- backendwebsiteservice/src/main/java/com/codurance/cephalopodwebsite/CephalopodWebsiteApplication.java
+++ backendwebsiteservice/src/main/java/com/codurance/cephalopodwebsite/CephalopodWebsiteApplication.java
@@ -2,13 +2,15 @@ package com.codurance.cephalopodwebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CephalopodWebsiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CephalopodWebsiteApplication.class, args);
